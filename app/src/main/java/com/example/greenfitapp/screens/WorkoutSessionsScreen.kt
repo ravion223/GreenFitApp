fd59d7724918aa9340dbed016f595274243c2a2e
Rostyslav <tablettaoson2016@gmail.com>
2026-02-28 18:40:42 +0200

    Added filter to classes

--- app/src/main/java/com/example/greenfitapp/screens/WorkoutSessionsScreen.kt
+++ app/src/main/java/com/example/greenfitapp/screens/WorkoutSessionsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Alarm
@@ -19,10 +20,15 @@ import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,9 +46,40 @@ import com.example.greenfitapp.ui.theme.GreenFitAppTheme
 
 @Composable
 fun WorkoutSessionsScreen(modifier: Modifier = Modifier){
-    LazyColumn() {
-        items(workoutSessionsList) { workout ->
-            WorkoutSessionCard(workout)
+    var selectedId by remember { mutableStateOf<Int?>(null) }
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+    ) {
+        LazyRow() {
+            item {
+                FilterChip(
+                    selected = selectedId == null,
+                    onClick = {
+                        selectedId = null
+                    },
+                    label = { Text("All") },
+
+                    )
+            }
+
+            items(locationsList) { location ->
+                FilterChip(
+                    selected = selectedId == location.id,
+                    onClick = {
+                        selectedId = location.id
+                    },
+                    label = { Text(stringResource(location.nameRes)) }
+                )
+            }
+        }
+
+        LazyColumn() {
+            val filteredList = if (selectedId == null) {workoutSessionsList}else{workoutSessionsList.filter { it.locationId == selectedId }}
+            items(filteredList) { workout ->
+                WorkoutSessionCard(workout)
+            }
         }
     }
 }
